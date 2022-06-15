576f2555010f300cd537f513e005de097ee6459f
Robert <rr.zinyat@gmail.com>
2022-06-15 17:50:21 +0300

    2 3 app layers

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -4,32 +4,26 @@ import java.time.LocalDateTime;
 
 public class MealTo {
 
+    private final Integer id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-//    private final AtomicBoolean excess;      // filteredByAtomic (or any ref type, e.g. boolean[1])
-//    private final Boolean excess;            // filteredByReflection
-//    private final Supplier<Boolean> excess;  // filteredByClosure
-    private boolean excess;
+    private final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
     }
 
-//    for filteredByClosure
-//    public Boolean getExcess() {
-//        return excess.get();
-//    }
-
-    // for filteredBySetterRecursion
-    public void setExcess(boolean excess) {
-        this.excess = excess;
+    public Integer getId() {
+        return id;
     }
 
     public LocalDateTime getDateTime() {
@@ -51,6 +45,7 @@ public class MealTo {
     @Override
     public String toString() {
         return "MealTo{" +
+                "id=" + id +
                 ", dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +

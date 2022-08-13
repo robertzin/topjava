506968baa7041c21f7acd162714f60a135f4628d
Robert <rr.zinyat@gmail.com>
2022-08-13 14:32:08 +0300

    10_02_HW9_test

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 @Controller
 public class RootController {
